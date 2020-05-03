bde25f0043e063f7e568f1420a86e68b4405bb21
szilvia77 <szilvia.goncalves@gmail.com>
2020-05-03 22:51:37 +0100

    03/05/ 2020 commit CMM007

--- gmail-contact.php
+++ gmail-contact.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /*
 THIS FILE USES PHPMAILER INSTEAD OF THE PHP MAIL() FUNCTION
 */
@@ -8,28 +9,30 @@ use PHPMailer\PHPMailer\PHPMailer;
 
 require './PHPMailer-master/vendor/autoload.php';
 
-
+/*
+*  CONFIGURE EVERYTHING HERE
+*/
 
 // an email address that will be in the From field of the email.
 $fromEmail = $_POST['email'];
 $fromName = $_POST['name'];
 
-// an email address & name that will receive the email with the output of the form
+// an email address that will receive the email with the output of the form
 $sendToEmail = 'sme.sos.uk@gmail.com';
-$sendToName = 'Sme-sos';
-
-// subject of the email
-$subject = 'New message from contact us';
-
+$sendToName = 'SME-sos';
 // form field names and their translations.
 // array variable name => Text to appear in the email
 $fields = array('name' => 'Name:', 'email' => 'Email:', 'message' => 'Message:');
 
-// message that will be displayed when everything is OK :)
+// message that will be displayed when everything is OK
 $okMessage = 'Successfully submitted - we will get back to you soon!';
 
-// If something goes wrong, we will display this message.
+// If something goes wrong, it will display this message.
 $errorMessage = 'There was an error while submitting the form. Please try again later';
+
+/*
+*  SENDING
+*/
 
 
 error_reporting(E_ALL & ~E_NOTICE);
@@ -37,7 +40,7 @@ error_reporting(E_ALL & ~E_NOTICE);
 try {
 
     if (count($_POST) == 0) throw new Exception('Form is empty');
-    $emailTextHtml .= "<h3>New message from sme-sos</h3><hr>";
+    $emailTextHtml .= "<h3>New message from smesos:</h3><hr>";
     $emailTextHtml .= "<table>";
 
     foreach ($_POST as $key => $value) {
@@ -47,19 +50,46 @@ try {
         }
     }
     $emailTextHtml .= "</table><hr>";
-    $emailTextHtml .= "<p>Have a great day!<br><br>Sincerely,<br><br>The SME-sos Team</p>";
+    $emailTextHtml .= "<p>Have a great day!<br><br>Sincerely,<br><br>SME-sos TEAM</p>";
 
     $mail = new PHPMailer;
 
-    $mail->setFrom($fromEmail, $fromName);
-    $mail->addAddress($sendToEmail, $sendToName); // add more addresses if needed by adding another line with $mail->addAddress();
-    $mail->addReplyTo($_POST['email'], $_POST['name']);
+//************** IMPORTANT
+
+    // write you email configuration below and see the important steps for it to work.
+
+    // IMPORTANT: You must turn off 2 step verification on your google/gmail account:
+    // Instructions: https://support.google.com/accounts/answer/1064203?co=GENIE.Platform%3DDesktop&hl=en
+    // Security Page: https://myaccount.google.com/security
+
+    // IMPORTANT You must also "Allow less secure apps" on your google/gmail account:
+    // https://myaccount.google.com/lesssecureapps
+
+//************** IMPORTANT
+
+    $mail->IsSMTP();
+    $mail->SMTPAuth = 'true';
+    $mail->SMTPSecure = 'ssl';
+    $mail->Host = 'smtp.gmail.com';
+    $mail->Port = "465"; // 8025, 587 and 25 can also be used. Use Port 465 for SSL.
+
+    $mail->Username = "sme.sos.uk@gmail.com";
+    $mail->Password = "smesos2020!";
+
+    $mail->From = $fromEmail;
+    $mail->FromName = $fromName;
+    $mail->AddAddress($sendToEmail);
+    $mail->AddReplyTo($fromEmail, $fromName);
 
 
-    $mail->Subject = $subject;
+    // end email configuration
+
+
+    $mail->Subject = 'New message from contact form';
 
     $mail->Body = $emailTextHtml;
     $mail->isHTML(true);
+    //$mail->msgHTML($emailTextHtml); // this will also create a plain-text version of the HTML email, very handy
 
 
     if (!$mail->send()) {
@@ -84,4 +114,3 @@ if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQ
 else {
     echo $responseArray['message'];
 }
-
